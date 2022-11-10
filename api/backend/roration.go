26e96288316c56aa1f35fe9ae031581daae6864a
王中阳 <wangzhongyang@stbz.net>
2022-11-10 15:40:07 +0800

    轮播图的更新_ORM链式操作的字段过滤

--- api/backend/roration.go
+++ api/backend/roration.go
@@ -23,9 +23,9 @@ type RotationDeleteRes struct{}
 
 type RotationUpdateReq struct {
 	g.Meta `path:"/backend/rotation/update/{Id}" method:"post" tags:"轮播图" summary:"修改轮播图接口"`
-	Id     uint   `json:"id"      v:"min:1#请选择需要修改的轮播图"  dc:"轮播图Id"`
-	PicUrl string `json:"pic_url" v:"required#图片链接不能为空"    dc:"图片地址"`
-	Link   string `json:"link"    v:"required#请输入跳转链接"      dc:"跳转链接"`
-	Sort   int    `json:"sort"    dc:"排序"`
+	Id     uint   `json:"id"      v:"min:1#请选择需要修改的轮播图" dc:"轮播图Id"`
+	PicUrl string `json:"pic_url" v:"required#轮播图图片链接不能为空" dc:"图片链接"`
+	Link   string `json:"link"    v:"required#跳转链接不能为空" dc:"跳转链接"`
+	Sort   int    `json:"sort"    dc:"跳转链接"`
 }
 type RotationUpdateRes struct{}
