55b443368d471d2e6c5b18b1f800c6fc75c942c9
Greenery_YAN <976443966@qq.com>
2023-03-22 15:53:57 +0800

    position getlist

--- internal/logic/position/position.go
+++ internal/logic/position/position.go
@@ -63,6 +63,30 @@ func (s *sPosition) Update(ctx context.Context, in model.PositionUpdateInput) er
 
 // GetList 查询内容列表
 func (s *sPosition) GetList(ctx context.Context, in model.PositionGetListInput) (out *model.PositionGetListOutput, err error) {
+	//1.获得*gdb.Model对象，方面后续调用
+	m := dao.PositionInfo.Ctx(ctx)
+	//2. 实例化响应结构体
+	out = &model.PositionGetListOutput{
+		Page: in.Page,
+		Size: in.Size,
+	}
+	//3. 分页查询
+	listModel := m.Page(in.Page, in.Size)
+	//4. 再查询count，判断有无数据
+	out.Total, err = m.Count()
+	if err != nil || out.Total == 0 {
+		return out, err
+	}
+	//5. 延迟初始化list切片 确定有数据，再按期望大小初始化切片容量
+	out.List = make([]model.PositionGetListOutputItem, 0, in.Size)
+	//6. 把查询到的结果赋值到响应结构体中
+	if err := listModel.Scan(&out.List); err != nil {
+		return out, err
+	}
+	return
+}
+
+func (s *sPosition) GetList_(ctx context.Context, in model.PositionGetListInput) (out *model.PositionGetListOutput, err error) {
 	var (
 		m = dao.PositionInfo.Ctx(ctx)
 	)
