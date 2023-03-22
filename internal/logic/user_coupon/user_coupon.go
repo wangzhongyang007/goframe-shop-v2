91c14a3ec2481a13e68967b2d1447729f5e94aee
Greenery_YAN <976443966@qq.com>
2023-03-22 16:12:43 +0800

    user_coupon getlist

--- internal/logic/user_coupon/user_coupon.go
+++ internal/logic/user_coupon/user_coupon.go
@@ -5,7 +5,6 @@ import (
 	"github.com/gogf/gf/v2/frame/g"
 	"goframe-shop-v2/internal/dao"
 	"goframe-shop-v2/internal/model"
-	"goframe-shop-v2/internal/model/entity"
 	"goframe-shop-v2/internal/service"
 )
 
@@ -51,30 +50,23 @@ func (s *sUserCoupon) Update(ctx context.Context, in model.UserCouponUpdateInput
 
 // GetList 查询分类列表
 func (s *sUserCoupon) GetList(ctx context.Context, in model.UserCouponGetListInput) (out *model.UserCouponGetListOutput, err error) {
-	var (
-		m = dao.UserCouponInfo.Ctx(ctx)
-	)
+	//1.获得*gdb.Model对象，方面后续调用
+	m := dao.UserCouponInfo.Ctx(ctx)
+	//2. 实例化响应结构体
 	out = &model.UserCouponGetListOutput{
 		Page: in.Page,
 		Size: in.Size,
 	}
-
-	// 分页查询
+	//3. 分页查询
 	listModel := m.Page(in.Page, in.Size)
-
-	// 执行查询
-	var list []*entity.UserCouponInfo
-	if err := listModel.Scan(&list); err != nil {
-		return out, err
-	}
-	// 没有数据
-	if len(list) == 0 {
-		return out, nil
-	}
+	//4. 再查询count，判断有无数据
 	out.Total, err = m.Count()
-	if err != nil {
+	if err != nil || out.Total == 0 {
 		return out, err
 	}
+	//5. 延迟初始化list切片 确定有数据，再按期望大小初始化切片容量
+	out.List = make([]model.UserCouponGetListOutputItem, 0, in.Size)
+	//6. 把查询到的结果赋值到响应结构体中
 	if err := listModel.Scan(&out.List); err != nil {
 		return out, err
 	}
