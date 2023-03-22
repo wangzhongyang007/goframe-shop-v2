551b64458a98ae6a0f2e5b203b54fe7578fc13b9
Greenery_YAN <976443966@qq.com>
2023-03-22 15:56:08 +0800

    position getlist

--- internal/logic/position/position.go
+++ internal/logic/position/position.go
@@ -7,7 +7,6 @@ import (
 	"github.com/gogf/gf/v2/frame/g"
 	"goframe-shop-v2/internal/dao"
 	"goframe-shop-v2/internal/model"
-	"goframe-shop-v2/internal/model/entity"
 	"goframe-shop-v2/internal/service"
 )
 
@@ -80,43 +79,6 @@ func (s *sPosition) GetList(ctx context.Context, in model.PositionGetListInput) 
 	//5. 延迟初始化list切片 确定有数据，再按期望大小初始化切片容量
 	out.List = make([]model.PositionGetListOutputItem, 0, in.Size)
 	//6. 把查询到的结果赋值到响应结构体中
-	if err := listModel.Scan(&out.List); err != nil {
-		return out, err
-	}
-	return
-}
-
-func (s *sPosition) GetList_(ctx context.Context, in model.PositionGetListInput) (out *model.PositionGetListOutput, err error) {
-	var (
-		m = dao.PositionInfo.Ctx(ctx)
-	)
-	out = &model.PositionGetListOutput{
-		Page: in.Page,
-		Size: in.Size,
-	}
-
-	// 分配查询
-	listModel := m.Page(in.Page, in.Size)
-	// 排序方式
-	listModel = listModel.OrderDesc(dao.PositionInfo.Columns().Sort)
-
-	// 执行查询
-	var list []*entity.PositionInfo
-	if err := listModel.Scan(&list); err != nil {
-		return out, err
-	}
-	// 没有数据
-	if len(list) == 0 {
-		return out, nil
-	}
-	out.Total, err = m.Count()
-	if err != nil {
-		return out, err
-	}
-	// Position
-	//指定item的键名用：ScanList
-	//if err := listModel.ScanList(&out.List, "Position"); err != nil {
-	//不指定item的键名用：Scan
 	if err := listModel.Scan(&out.List); err != nil {
 		return out, err
 	}
